60129d824b84124e30eb79318de4c98b8fc1bb9c
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-09-06 16:05:53 -0500

    Succesfully refactor code by express

--- index.js
+++ index.js
@@ -1,56 +1,52 @@
-const http = require("http");
+const express = require("express");
 const path = require("path");
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  const filePath = path.join(
-    __dirname,
-    "pages",
-    req.url === "/" ? "index.html" : req.url
-  );
-  console.log(filePath);
+// Start an Express application
+const app = express();
 
-  const pathExt = path.extname(filePath);
+// Define port to listen
+const PORT = 3000;
 
-  let contentType = "text/html";
-  switch (pathExt) {
-    case ".css":
-      contentType = "text/css";
-      break;
-    case ".js":
-      contentType = "text/js";
-      break;
-    case ".json":
-      contentType = "application/json";
-      break;
-    case ".png":
-      contentType = "image/png";
-      break;
-  }
+// Define what to return on get requests
 
-  // Read file async
+app.get("/", (req, res) => {
+  const filePath = path.join(__dirname, "pages", "index.html");
 
-  fs.readFile(filePath, (err, content) => {
-    // Handle Errors
-    if (err) {
-      if (err.code == "ENOENT") {
-        fs.readFile(
-          path.join(__dirname, "pages", "404.html"),
-          (err, content) => {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(content, "utf-8");
-          }
-        );
-      } else {
-        console.log(err);
-        throw err;
-      }
-    }
-    res.writeHead(200, { "Content-Type": contentType });
-    res.end(content);
-  });
+  res.sendFile(filePath);
 });
 
-const PORT = process.env.port || 5000;
+app.get("/about.html", (req, res) => {
+  const filePath = path.join(__dirname, "pages", "about.html");
 
-server.listen(PORT);
+  res.sendFile(filePath);
+});
+
+app.get("/contact-me.html", (req, res) => {
+  const filePath = path.join(__dirname, "pages", "contact-me.html");
+
+  res.sendFile(filePath);
+});
+
+app.get("/404", (req, res) => {
+  const filePath = path.join(__dirname, "pages", "index.html");
+
+  res.sendFile(filePath);
+});
+
+/* This route will handle all the requests that are 
+   not handled by any other route handler. In 
+   this handler we will redirect the user to 
+   an error page with NOT FOUND message and status
+   code as 404 (HTTP status code for NOT found) 
+*/
+
+app.all("*", (req, res) => {
+  const filePath = path.join(__dirname, "pages", "404.html");
+  res.sendFile(filePath);
+});
+
+// Listen to established port
+
+app.listen(PORT, () => {
+  console.log("Basic server listening to " + PORT);
+});
